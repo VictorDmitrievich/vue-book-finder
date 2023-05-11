0a14c6ea785ab4d444d0e8fc3745843a9c493b30
VictorDmitrievich <126975156+VictorDmitrievich@users.noreply.github.com>
2023-05-11 15:32:20 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'а
+import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSignInAlt} from '@fortawesome/free-solid-svg-icons'
